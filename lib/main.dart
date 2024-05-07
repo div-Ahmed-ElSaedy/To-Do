74bb0f5c50bca49983c1915fe756e3fdc3cf3f72
ElSaedy <dev.ahmed.elsaedy@gmail.com>
2024-05-07 22:57:37 +0300

    Register Adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_app/widgets/constants.dart';
+import 'models/task_model.dart';
 import 'views/Home_view.dart';
 
 void main() async {
   await Hive.initFlutter();
 
   await Hive.openBox(kTaskBox);
-  
+  Hive.registerAdapter(TaskModelAdapter());
   runApp(const ToDoApp());
 }
 
